e8af9d3ae4062958f4056217ed8216860615c1cc
nadws <nandw567@gmail.com>
2023-08-04 07:53:28 +0800

    tes

---
+++ app/Http/Controllers/Stok_ayamController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Stok_ayamController extends Controller
+{
+    public function index(Request $r)
+    {
+        $data = [
+            'title' => 'total'
+        ];
+    }
+}
