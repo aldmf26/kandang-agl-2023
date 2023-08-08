4e5ef05497057f59fc46302705705cb321a3b403
aldmf26 <aldimf26@gmail.com>
2023-08-08 09:10:17 +0800

    123

--- app/Http/Controllers/DashboardKandangController.php
+++ app/Http/Controllers/DashboardKandangController.php
@@ -841,8 +841,7 @@ class DashboardKandangController extends Controller
 
     public function tambah_perencanaan(Request $r)
     {
-        // $tgl = $r->tgl;
-        $tgl = date('Y-m-d');
+        $tgl = $r->tgl;
         $id_kandang = $r->id_kandang;
         $kg_pakan_box = $r->kg_pakan_box;
         $populasi = $r->populasi;
