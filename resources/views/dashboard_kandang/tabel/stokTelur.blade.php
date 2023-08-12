d0a3b74ef81c656bad53f563e0ecad8ecf25fa8f
aldmf26 <aldimf26@gmail.com>
2023-08-12 14:41:41 +0800

    123

--- resources/views/dashboard_kandang/tabel/stokTelur.blade.php
+++ resources/views/dashboard_kandang/tabel/stokTelur.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
     @if (session()->has('errorMin'))
-    <x-theme.alert pesan="ada input yang salah !" />
+        <x-theme.alert pesan="ada input yang salah !" />
     @endif
     <div class="col-lg-12">
         <h6>
@@ -8,52 +8,54 @@
         </h6>
         <table class="table table-bordered text-center">
             @php
-            $ttlPcs = 0;
-            $ttlKg = 0;
-            $ttlIkat = 0;
+                $ttlPcs = 0;
+                $ttlKg = 0;
+                $ttlIkat = 0;
             @endphp
             <tr>
                 <th class="dhead" rowspan="2" style="vertical-align: middle">Gudang</th>
                 @foreach ($telur as $d)
-                <th class="dhead" colspan="3">
-                    {{ ucwords(str_replace('telur', '', strtolower($d->nm_telur))) }}</th>
+                    <th class="dhead" colspan="3">
+                        {{ ucwords(str_replace('telur', '', strtolower($d->nm_telur))) }}</th>
                 @endforeach
             </tr>
 
             <tr>
                 @php
-                $telur = DB::table('telur_produk')->get();
+                    $telur = DB::table('telur_produk')->get();
                 @endphp
                 @foreach ($telur as $d)
-                <th class="dhead text-end">Pcs</th>
-                <th class="dhead text-end">Kg</th>
-                <th class="dhead text-end">Ikat</th>
+                    <th class="dhead text-end">Pcs</th>
+                    <th class="dhead text-end">Kg</th>
+                    <th class="dhead text-end">Ikat</th>
                 @endforeach
             </tr>
             <tr>
                 <td align="left">
                     Martadah
-                    <a href="{{ route('opnamemtd',['id_gudang' => '1']) }}"
+                    <a href="{{ route('opnamemtd', ['id_gudang' => '1']) }}"
                         class="badge float-end me-2 bg-primary text-sm">Opname</a>
                     <a data-bs-toggle="modal" data-bs-target="#history_opn" href="#"
                         class="badge float-end me-2 bg-primary text-sm history_opname">History Opname</a>
                 </td>
                 @foreach ($telur as $d)
-                @php
-                $stok = DB::selectOne("SELECT SUM(pcs - pcs_kredit) as pcs, SUM(kg - kg_kredit) as kg FROM
+                    @php
+                        $stok = DB::selectOne("SELECT SUM(pcs - pcs_kredit) as pcs, SUM(kg - kg_kredit) as kg FROM
                 `stok_telur`
                 WHERE id_telur = '$d->id_produk_telur' AND id_gudang = 1 AND opname = 'T';");
-                request()
-                ->session()
-                ->forget('errorMin', '1');
-                if ($stok->pcs < 0) { request() ->session()
-                    ->put('errorMin', '1');
-                    }
+                        request()
+                            ->session()
+                            ->forget('errorMin', '1');
+                        if ($stok->pcs < 0) {
+                            request()
+                                ->session()
+                                ->put('errorMin', '1');
+                        }
                     @endphp
                     <td align="right">{{ number_format($stok->pcs, 0) }}</td>
                     <td align="right">{{ number_format($stok->kg, 0) }}</td>
                     <td align="right">{{ number_format($stok->pcs / 180, 0) }}</td>
-                    @endforeach
+                @endforeach
             </tr>
             <tr>
                 <td align="left">
@@ -67,21 +69,21 @@
                     </a>
                 </td>
                 @foreach ($telur as $d)
-                @php
-                $stok = DB::selectOne("SELECT SUM(pcs_kredit) as pcs, SUM(kg_kredit) as kg FROM `stok_telur`
+                    @php
+                        $stok = DB::selectOne("SELECT SUM(pcs_kredit) as pcs, SUM(kg_kredit) as kg FROM `stok_telur`
                 WHERE id_telur = '$d->id_produk_telur' AND jenis = 'penjualan' AND opname = 'T';");
-
-                @endphp
-                <td align="right">{{ number_format($stok->pcs, 0) ?? 0 }}</td>
-                <td align="right">{{ number_format($stok->kg, 0) ?? 0 }}</td>
-                <td align="right">{{ number_format($stok->pcs / 180, 0) }}</td>
+                        
+                    @endphp
+                    <td align="right">{{ number_format($stok->pcs, 0) ?? 0 }}</td>
+                    <td align="right">{{ number_format($stok->kg, 0) ?? 0 }}</td>
+                    <td align="right">{{ number_format($stok->pcs / 180, 0) }}</td>
                 @endforeach
             </tr>
             <tr>
                 <td align="left">
                     Transfer Alpa
-                    <a href="{{ route('opnamemtd',['id_gudang' => '2']) }}"
-                        class="badge float-end me-2 bg-primary text-sm">Opname</a>
+                    {{-- <a href="{{ route('opnamemtd', ['id_gudang' => '2']) }}"
+                        class="badge float-end me-2 bg-primary text-sm">Opname</a> --}}
                     <a data-bs-toggle="tooltip" data-bs-placement="top" title="Tambah Transfer Alpa"
                         href="{{ route('dashboard_kandang.add_transfer_stok', ['id_gudang' => 1]) }}"
                         class="badge float-end me-2 bg-primary text-sm"><i class="fas fa-plus"></i></a>
@@ -93,15 +95,15 @@
 
                 </td>
                 @foreach ($telur as $d)
-                @php
-                $stok = DB::selectOne("SELECT SUM(pcs - pcs_kredit) as pcs, SUM(kg - kg_kredit) as kg FROM
+                    @php
+                        $stok = DB::selectOne("SELECT SUM(pcs - pcs_kredit) as pcs, SUM(kg - kg_kredit) as kg FROM
                 `stok_telur`
                 WHERE id_telur = '$d->id_produk_telur' AND id_gudang = 2 AND opname = 'T'");
-
-                @endphp
-                <td align="right">{{ number_format($stok->pcs) ?? 0 }}</td>
-                <td align="right">{{ number_format($stok->kg, 0) ?? 0 }}</td>
-                <td align="right">{{ number_format($stok->pcs / 180, 0) }}</td>
+                        
+                    @endphp
+                    <td align="right">{{ number_format($stok->pcs) ?? 0 }}</td>
+                    <td align="right">{{ number_format($stok->kg, 1) ?? 0 }}</td>
+                    <td align="right">{{ number_format($stok->pcs / 180, 0) }}</td>
                 @endforeach
             </tr>
         </table>
