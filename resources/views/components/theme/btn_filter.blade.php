5bd2282feb35b2d4de0cecc03345e862964c2833
nadws <nandw567@gmail.com>
2025-04-10 15:57:26 +0800

    update btn_filter.blade.php and penjualan_telur.blade.php

--- resources/views/components/theme/btn_filter.blade.php
+++ resources/views/components/theme/btn_filter.blade.php
@@ -42,6 +42,7 @@
                     <option value="2022">2022</option>
                     <option value="2023">2023</option>
                     <option value="2024">2024</option>
+                    <option value="2025">2025</option>
                 </select>
             </div>
             <div class="col-lg-8 tahun_muncul mt-2">
@@ -50,6 +51,7 @@
                     <option value="2022">2022</option>
                     <option value="2023">2023</option>
                     <option value="2024">2024</option>
+                    <option value="2025">2025</option>
                 </select>
             </div>
         </div>
